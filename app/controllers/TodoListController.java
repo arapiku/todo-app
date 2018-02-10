96f7892a14157658cb1788e97a48fd3bdefeb4de
arapiku <arpaiku420@gmail.com>
2018-02-11 01:57:38 +0900

    setItem, edit, updateメソッドを追加

--- app/controllers/TodoListController.java
+++ app/controllers/TodoListController.java
@@ -7,7 +7,7 @@ import views.html.*;
 
 import java.util.List;
 
-import static play.data.Form.*;
+import static play.data.Form.form;
 
 public class TodoListController extends Controller {
 
@@ -36,6 +36,39 @@ public class TodoListController extends Controller {
             return redirect("/");
         } else {
             return badRequest(add.render(f));
+        }
+    }
+
+    public Result setItem() {
+        Form<TodoList> f = form(TodoList.class);
+        return ok(single.render("ID番号を入力", f));
+    }
+
+    public Result edit() {
+        Form<TodoList> f = form(TodoList.class).bindFromRequest();
+        if (!f.hasErrors()) {
+            TodoList obj = f.get();
+            Long id = obj.id;
+            obj = TodoList.find.byId(id);
+            if (obj != null) {
+                f = form(TodoList.class).fill(obj);
+                return ok(edit.render("ID=" + id + "の投稿を編集。", f));
+            } else {
+                return ok(single.render("ERROR:IDの投稿が見つかりません。", f));
+            }
+        } else {
+            return ok(single.render("ERROR:入力に問題があります。", f));
+        }
+    }
+
+    public Result update() {
+        Form<TodoList> f = form(TodoList.class).bindFromRequest();
+        if (!f.hasErrors()) {
+            TodoList data = f.get();
+            data.update();
+            return redirect("/");
+        } else {
+            return ok(edit.render("ERROR:再度入力してください。", f));
         }
     }
 }
